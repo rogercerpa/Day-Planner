216ba2adcc6c75b50fd396c1e6f4b786e15c6ba6
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-01-10 22:07:06 -0500

    modified the js file for the save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,19 +8,26 @@ function dateTime() {
 dateTime();
 
 //SAVE BUTTON CODE - SAVING TASK TO LOCAL STORAGE
-var saveButton9am = document.getElementById('#btn9am');
-var taskinput = document.querySelector('#task9am');
+var saveButton9am = $('#btn9am');
+var taskinput = $('#task9am').val();
 
 $('#btn9am').on('click', function(savetask) {
 	savetask.preventDefault();
 
 	var task = taskinput.value.trim();
 
-	localStorage.setItem('task', JSON.stringify(task));
-	// if (typeof Storage !== 'undefined') {
+	localStorage.setItem('input9am', taskinput);
+
+	// if (taskinput === '') {
 	// 	localStorage.setItem('task', JSON.stringify(task));
-	// 	localStorage.getItem('task');
+	// 	// localStorage.getItem('task');
 	// } else {
-	// 	displayMessage('error', 'Enter your task here...');
+	// 	displayMessage('task', 'Enter your task here...');
 	// }
+	// renderLastRegistered();
 });
+
+//Row color changing code
+var rowgreen = document.querySelector('.future');
+var rowred = document.querySelector('.past');
+var rowgray = document.querySelector('.preset');
