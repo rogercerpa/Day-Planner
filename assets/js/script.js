6f524ceb82a64f87c39b6da3a6cc9b13c82552ce
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-01-11 21:43:00 -0500

    js code for the task color changing, not working yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,16 @@ function dateTime() {
 dateTime();
 
 //SAVE BUTTON CODE - SAVING TASK TO LOCAL STORAGE
-var saveButton9am = $('#btn9am');
-var taskinput = $('#task9am').val();
+// var saveButton9am = $('#btn9am');
+// var taskinput = $('#task9am').val();
 
-$('#btn9am').on('click', function(savetask) {
+$('button').on('click', function(savetask) {
 	savetask.preventDefault();
-
-	var task = taskinput.value.trim();
-
-	localStorage.setItem('input9am', taskinput);
+	var taskinput = $('textarea').val();
+	console.log(taskinput);
+	// var data = $(this).val(JSON.stringify('data-task'));
+	// localStorage.setItem(this, data);
+	// localStorage.getItem('task');
 
 	// if (taskinput === '') {
 	// 	localStorage.setItem('task', JSON.stringify(task));
@@ -28,6 +29,26 @@ $('#btn9am').on('click', function(savetask) {
 });
 
 //Row color changing code
-var rowgreen = document.querySelector('.future');
-var rowred = document.querySelector('.past');
-var rowgray = document.querySelector('.preset');
+
+// var rowgreen = document.querySelector('.future');
+// var rowred = document.querySelector('.past');
+// var rowgray = document.querySelector('.preset');
+
+var time = moment();
+var format = 'hh:mm:ss';
+var time8am = moment('8:00:00', format);
+var time9am = moment('09:00:00', format);
+var time10am = moment('10:00:00', format);
+var time11am = moment('11:00:00', format);
+
+var task9am = $('#task9am');
+
+// if (time.isBetween(time9am, time10am)) {
+// 	$('#task9am').addClass('present');
+// }
+if (time.isBetween(time8am, time9am)) {
+	$('#task9am').addClass('past');
+}
+// else if (time.isBetween(time10am, time11am)) {
+// 	$('#task9am').addClass('future');
+// }
